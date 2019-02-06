ff911007b80c27ee6473522f3294465b8c8d91a4
Darag1996 <moha.darag@yahoo.com>
2019-02-06 20:39:07 +0200

    Fixing Tag issue

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,27 +3,26 @@
       <h4>Archives</h4>
       <ol class="list-unstyled">
         @foreach($archives as $stats) 
-           <li>
-              <a href="/?month={{ $stats['month'] }}&year={{ $stats['year'] }}">
-                {{ $stats['month'] . ' ' . $stats['year'] }}
-              </a>
-          </li>
+        <li>
+          <a href="/?month={{ $stats['month'] }}&year={{ $stats['year'] }}">
+            {{ $stats['month'] . ' ' . $stats['year'] }}
+          </a>
+        </li>
         @endforeach
       </ol>
-    </div>
-  </div>
-
-    <div class="col-sm-3 offset-sm-1 blog-sidebar ml-auto">
-    <div class="sidebar-module">
+      <hr>
+      
       <h4>Tags</h4>
       <ol class="list-unstyled">
         @foreach($tags as $tag) 
-           <li>
-              <a href="/posts/tags/{{ $tag }}">
-                {{ $tag }}
-              </a>
-          </li>
+        <li>
+          <a href="/posts/tags/{{ $tag }}">
+            {{ $tag }}
+          </a>
+        </li>
         @endforeach
       </ol>
+
     </div>
   </div>
+
